2ed527066ce268a578e6b434c47c22c0e6fc4df4
Baiur Yuliia <juliy14497@gmail.com>
2024-10-30 14:46:29 +0200

    Add buttons

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,13 +7,34 @@ from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
 
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    button_site = types.KeyboardButton("Перейти на сайт")
+    button_delete = types.KeyboardButton("Удалить фото")
+    button_edit = types.KeyboardButton("Изменить фото")
+    markup.row(button_site)
+    markup.row(button_delete, button_edit)
+    bot.send_message(message.chat.id, "Какое красивое фото!", reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
+
+
+def on_click(message):
+    if message.text == "Перейти на сайт":
+        webbrowser.open("https://translate.google.com/?sl=en&tl=ru&op=translate")
+    elif message.text == "Удалить фото":
+        bot.send_message(message.chat.id, "Delete photo")
+    elif message.text == "Изменить фото":
+        bot.send_message(message.chat.id, "Edit photo")
+
+
 @bot.message_handler(content_types=["photo"])
 def get_photo(message):
     markup = types.InlineKeyboardMarkup()
     button_site = types.InlineKeyboardButton(
-            "Перейти на сайт",
-            url="https://translate.google.com/?sl=en&tl=ru&op=translate",
-        )
+        "Перейти на сайт",
+        url="https://translate.google.com/?sl=en&tl=ru&op=translate",
+    )
     button_delete = types.InlineKeyboardButton("Удалить фото", callback_data="delete")
     button_edit = types.InlineKeyboardButton("Изменить текст", callback_data="edit")
     markup.row(button_site)
@@ -23,17 +44,12 @@ def get_photo(message):
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
-    if callback.data == 'delete':
+    if callback.data == "delete":
         bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
-    elif callback.data == 'edit':
-        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
-
-
-
-
-@bot.message_handler(commands=["start"])
-def main(message):
-    bot.send_message(message.chat.id, "Text")
+    elif callback.data == "edit":
+        bot.edit_message_text(
+            "Edit text", callback.message.chat.id, callback.message.message_id
+        )
 
 
 @bot.message_handler(commands=["help"])
